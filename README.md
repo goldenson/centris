da45cef5e0a19744b2ba0eea6231f10222273036
Maxime Orefice <maxime.orefice@shopify.com>
2019-03-27 20:57:14 -0400

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# centris
+## Centris
+
+### Todo
+
+- [ ] Fetch data on check with postman
+- [ ] Process usefull data
+- [ ] Fetch info on properties
+- [ ] Persist them on the system if do not exist
+- [ ] Display them on an app
--- centris.rb
+++ centris.rb
@@ -1,11 +1,5 @@
 require 'json'
 
-# 1. Fetch data on check with postman
-# 2. Process usefull data
-# 3. Fetch info on properties
-# 4. Persist them on the system if do not exist
-# 5. Display them on an app
-
 file = File.read('markers.json')
 markers = JSON.parse(file)
 
